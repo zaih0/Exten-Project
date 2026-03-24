b8fb13f2f9aa822bfa0fd852c7189b031e5dbcf9
MeAndMonke <525575@vistacollege.nl>
2026-03-24 12:47:14 +0100

    docker ongeveer werk

--- kunstkwartiertje/src/app/Login.tsx
+++ kunstkwartiertje/src/app/Login.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function login() {
   return (
     // login screen
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
