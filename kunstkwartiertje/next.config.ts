dd9fc5d599d547acaf81c110f4ecbc7c4fd65bb5
pmagicguy <402919@vistacollege.nl>
2026-04-01 12:35:41 +0200

    Revert "merge conflict fix???"
    This reverts commit a26059a3d4566fb81b861fdd8baab5c58185de2c.

--- kunstkwartiertje/next.config.ts
+++ kunstkwartiertje/next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   turbopack: {
+<<<<<<< HEAD
+    root: __dirname,
+=======
     root: process.cwd(),
+>>>>>>> bbbfee9d73ff71b31a985eb4f7a46ab080fb97c6
   },
 };
 
