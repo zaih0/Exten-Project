27ec8f74d1e8f7b413c44dc1b241ed2cf97a473c
zaih0 <king.zh100@gmail.com>
2026-04-14 14:44:33 +0200

    Admin fix

--- kunstkwartiertje/next.config.ts
+++ kunstkwartiertje/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/Admin",
+        destination: "/admin",
+        permanent: true,
+      },
+      {
+        source: "/Admin/:path*",
+        destination: "/admin/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
