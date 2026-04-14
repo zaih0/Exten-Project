2155dfa2dc7d6263d73544a1c8f99e9bd7528c04
zaih0 <king.zh100@gmail.com>
2026-04-14 19:04:27 +0200

    admin

--- kunstkwartiertje/next.config.ts
+++ kunstkwartiertje/next.config.ts
@@ -2,20 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: "/Admin",
-        destination: "/admin",
-        permanent: true,
-      },
-      {
-        source: "/Admin/:path*",
-        destination: "/admin/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
